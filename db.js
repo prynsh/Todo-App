439d0b5d688b1c09f9bd774948e6d64588abb14c
Priyansh Verma <priyansh2112@gmail.com>
2024-03-28 18:10:26 +0530

    added db

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://prynsh:Priyal2112@cluster0.vi5pepe.mongodb.net/todos");
+
+const todoSchema=mongoose.Schema({
+    title:String,
+    description: String,
+    completed:Boolean,
+})
+
+const todo= mongoose.model("todos", todoSchema);
+module.exports({
+    todo:todo
+})
